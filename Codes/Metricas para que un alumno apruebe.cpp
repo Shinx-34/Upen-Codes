fd488ce65565c6ce9af10c426cb526db7de19ac3
Shinx-34 <shinx398@gmail.com>
2026-06-09 08:42:29 -0700

    Update approval criteria and messages in grading program

--- Codes/Metricas para que un alumno apruebe.cpp
+++ Codes/Metricas para que un alumno apruebe.cpp
@@ -2,9 +2,9 @@
 using namespace std; //Iostream y este elemento son necesarios para que cin y cout sean funcionales 
 
 int main(){
-    float trabajo, portafolio, examen, asistencia, preex, final; //variables para cada cosa, pre-ex es para calcular si hay permiso para el examen.
+    float trabajo, portafolio, examen, asistencia, preex, final; //variables para cada cosa, preex es para calcular si hay permiso para el examen.
 
-    cout << "Inserta tu calificacion de trabajo:"; //Cout obtiene la informacion
+    cout << "Inserta tu calificacion de trabajo:"; //Cout muestra la informacion como lo haria un PrintF
     cin >> trabajo; //cin se la otorga a la variable
 
     cout << "inserta tu calificacion del portafolio:";
@@ -19,12 +19,12 @@ int main(){
     preex =  (trabajo * 0.30) + (portafolio * 0.30) + (examen * 0.40);
 
     if (trabajo >= 50 && portafolio >= 50 && examen >= 50 && asistencia >= 80){ //calculo para ambos requisitos 
-    cout << "Apto para evaluacion ordinaria";//cout pose una 2nda funcion que le permite desplegar mensajes se puede usar como variante del Printf
+    cout << "Apto para evaluacion ordinaria";
     
     final =  preex; //calculo final para ir o no remedial
 
-        //calculo para aprobar o no el curso, si es mayor o igual a 60 se aprueba, si no se va a remedial
-    if (final >= 60){
+        //calculo para aprobar o no el curso, si es mayor o igual a 70 se aprueba, si no se va a remedial
+    if (final >= 70){
         cout << "\nAlumno Aprobado! calificacion final:\n";
         cout << final;
     }
@@ -34,7 +34,7 @@ int main(){
     }
     }
     else{//el que pasa si no se cumplen las condiciones
-        cout<<"Alumno a remedial\n"; 
+        cout<<"Alumno a remedial debido que una de las condiciones no se cumplio\n"; 
         cout << preex;
     }
 
